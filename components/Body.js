07099eaf5138f8546c8c6fb7e5be6f4b023a167b
DHIRAJB7 <dhiraj.basavaraju@gmail.com>
2023-03-07 04:31:57 +0530

    added no task or all task completed scenatio

--- components/Body.js
+++ components/Body.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
-import { StyleSheet, View, FlatList, Alert } from "react-native";
+import { StyleSheet, View, FlatList, Alert, Text } from "react-native";
 import ToDoItem from "./ToDoItem";
 import ToDoAdder from "./ToDoAdder";
 
 const Body = () => {
   const [todo, settodo] = useState([
-    { text: "code today", id: 1 },
-    { text: "sleep", id: 2 },
-    { text: "play xbox", id: 3 },
+    { text: "eat", id: 1 },
+    { text: "code", id: 2 },
+    { text: "sleep", id: 3 },
   ]);
 
   const keyPressed = (id) => {
@@ -15,26 +15,37 @@ const Body = () => {
   };
 
   const updateTask = (task) => {
-    if(todo.map(a => a.text).includes(String(task).toLowerCase())){
-        Alert.alert("Hmmm!!!","Task already present in list.",[{text:"understood"}])
-    }else{
-        const id = [...todo.map((item) => item.id).sort()].pop() + 1;
-        settodo([...todo, { text: task, id }]);
+    if (todo.map((a) => a.text).includes(String(task).toLowerCase())) {
+      Alert.alert("Hmmm!!!", "Task already present in list.", [
+        { text: "understood" },
+      ]);
+    } else {
+      let id = [...todo.map((item) => item.id).sort()].pop();
+      id = id ? id + 1 : 1;
+      settodo([...todo, { text: task, id }]);
+      console.log(task, id);
     }
   };
 
   return (
     <View style={styles.bodyContainer}>
-      <View style={styles.addContainer}>
+      <View>
         <ToDoAdder updateTask={updateTask} />
       </View>
       <View style={styles.listContainer}>
-        <FlatList
-          data={todo}
-          renderItem={({ item }) => (
-            <ToDoItem item={item} keyPressed={keyPressed} />
-          )}
-        />
+        {todo.length > 0 ? (
+          <FlatList
+            data={todo}
+            renderItem={({ item }) => (
+              <ToDoItem item={item} keyPressed={keyPressed} />
+            )}
+          />
+        ) : (
+          <View>
+            <Text style={styles.noTaskMessage1}>You have completed all tasks.</Text>
+            <Text style={styles.noTaskMessage2}>Psst!! You can still do many tasks.</Text>
+          </View>
+        )}
       </View>
     </View>
   );
@@ -42,13 +53,24 @@ const Body = () => {
 
 const styles = StyleSheet.create({
   bodyContainer: {
-    flex:1,
+    flex: 1,
     padding: 16,
   },
-  addContainer: {
+  noTaskMessage1: {
+    fontSize: 28,
+    color:"coral",
+    marginTop:10,
+    textAlign:"center"
+
+  },
+  noTaskMessage2: {
+    fontSize: 24,
+    color:"teal",
+    marginTop:16,
+    textAlign:"center"
   },
   listContainer: {
-    flex:1,
+    flex: 1,
   },
 });
 
